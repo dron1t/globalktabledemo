f1f4d1a32e04aa93c8d92ca8f64cf32561418eb7
lkedron <lkedron@gmail.com>
2023-03-19 18:48:50 +0100

    adding time logging

--- src/main/java/com/kainos/globalktabledemo/RedisController.java
+++ src/main/java/com/kainos/globalktabledemo/RedisController.java
@@ -3,7 +3,6 @@ package com.kainos.globalktabledemo;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
